2c57d87c96564fffd03fcac55a7c1e5a648822f8
Sougata Patra <rikpatra8@gmail.com>
2026-04-24 23:35:27 +0530

    Contact Added

---
+++ Guide_finder_platform-main/Backend/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Import Guide Model
+const Guide = require("./models/Guide");
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Guide Finder Backend Running");
+});
+
+// Get all guides
+app.get("/guides", async (req, res) => {
+  try {
+    const guides = await Guide.find();
+    res.json(guides);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Add new guide
+app.post("/guides", async (req, res) => {
+  try {
+    const guide = new Guide(req.body);
+    const savedGuide = await guide.save();
+    res.status(201).json(savedGuide);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// MongoDB Connection
+mongoose.connect("mongodb://127.0.0.1:27017/guidefinder")
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Start server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
